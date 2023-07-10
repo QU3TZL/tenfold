377350f63146dd72e4e9577ee2556e089e6151d7
Quetzl <112727259+QU3TZL@users.noreply.github.com>
2023-07-10 17:04:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const filepondElements = document.querySelectorAll('.filepond');
+
+  filepondElements.forEach(function(element, index) {
+    FilePond.create(element, {
+      server: {
+        url: 'https://your-server-side-endpoint',
+        process: {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          onload: response => console.log(response),
+          onerror: error => console.log(error),
+          ondata: formData => {
+            formData.append('folder', 'folder' + (index + 1));
+            return formData;
+          }
+        }
+      },
+      allowMultiple: true
+    });
+  });
+});

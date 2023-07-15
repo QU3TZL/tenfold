fc71609b6cd187953e73061f42657ea665e1226b
Quetzl <112727259+QU3TZL@users.noreply.github.com>
2023-07-15 12:46:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-const filePondInstances = document.querySelectorAll('.filepond');
+const FilePond = require('filepond');
+require('filepond-plugin-file-validate-type');
+require('filepond-plugin-file-validate-size');
+require('filepond-plugin-file-poster');
+require('filepond-plugin-image-preview');
 
-filePondInstances.forEach((inputElement, index) => {
-  const pond = FilePond.create(inputElement, {
+// Fetch all the FilePond elements and initialize them
+const filePonds = document.querySelectorAll('.filepond');
+
+// Convert NodeList to an Array
+const filePondArray = Array.from(filePonds);
+
+filePondArray.forEach((fp, index) => {
+  const pond = FilePond.create(fp, {
+    name: `filepond${index + 1}`,
     server: {
       url: 'https://renderdisk.onrender.com/upload',
-      process: {
-        url: `/folder${index+1}`,
-        method: 'POST'
-      }
+      process: `/${index + 1}`
+    },
+    labelIdle: 'Drop your files here or <span class="filepond--label-action"> Browse </span>',
+  });
+
+  // Listen to addfile event to update table
+  pond.on('addfile', (error, file) => {
+    if (error) {
+      console.log('Add file failed', error);
+      return;
     }
+    
+    // If no error, update the table
+    const table = document.querySelector(`#table${index + 1}`);
+    const row = table.insertRow(-1);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    
+    // Date added
+    const currentDate = new Date();
+
+    cell1.innerHTML = file.filename;
+    cell2.innerHTML = file.fileSize;
+    cell3.innerHTML = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
   });
 });
